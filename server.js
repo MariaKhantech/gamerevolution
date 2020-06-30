a877c219c613ac2c0fe5aae9aa29a95f306d3ea9
hank amadeus <hankamadeus@email.com>
2020-06-30 19:41:06 -0400

    added comment to server.js for express midddleware/created gus-branch

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const app = express();
 
 const PORT = process.env.PORT || 8080;
 
+// Middleware for express
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
