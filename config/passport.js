42aaa7dc206d50463b5d0704efda022ca36d0797
Shannon Trainor <sltrainor@outlook.com>
2020-07-06 11:43:53 -0400

    added passport syntax

--- config/passport.js
+++ config/passport.js
@@ -1,3 +1,32 @@
 const passport = require("passport");
-const LocalStrategy = require("passport-local").Strategy;
+LocalStrategy = require("passport-local").Strategy;
 
+
+passport.use(new LocalStrategy (
+    (username, password, done) => {
+        User.findOne({username: username}, (err, user) => {
+            if(err) {
+                return done (err);
+            };
+            if (!user) {
+                return done (null, false, {message: "Incorrect Usrname"});
+            };
+            if (!user.validPassword(password)) {
+                return done(null, false, {message: "Incorrect Password"})
+            }
+            return done (null, user);
+        });
+    }
+));
+
+//serialize/de to keep authentication consistent
+passport.serializeUser((user, cb) => {
+    cb(null, user);
+});
+
+passport.deserializeUser((obj, cb) => {
+    cb(null, obj);
+});
+
+//export
+module.exports = passport;
