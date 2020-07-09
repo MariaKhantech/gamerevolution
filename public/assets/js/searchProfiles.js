67c78e8ceda208aaef614c529edf2980afa95035
mmkhan <mariatechfuture@gmail.com>
2020-07-09 13:09:26 -0400

    fixed issue with search profile carousel

--- public/assets/js/searchProfiles.js
+++ public/assets/js/searchProfiles.js
@@ -1,15 +1,16 @@
 
+$(document).ready(() => {
 
-$("#carousel").Cloud9Carousel( {
+  $("#carousel").Cloud9Carousel( {
     buttonLeft: $("#buttons > .left"),
     buttonRight: $("#buttons > .right"),
     autoPlay: 1,
     bringToFront: true,
-  
-    mirror: {
-      gap: 12,     /* 12 pixel gap between item and reflection */
-      height: 0.2, /* 20% of item height */
-      opacity: 0.4 /* 40% opacity at the top */
-    }
-    
-  } );
+    yRadius:100,
+    xRadius: 600
+ 
+
+  });
+
+});
+
